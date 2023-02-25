8ddbcfee8b6ba75be6dec63152d5507890b10cc0
Deniz Groenhoff <deniz.groenhoff@uni-oldenburg.de>
2023-02-25 23:32:51 +0100

    feat(Logger): Added possibility to force multibyte encoding

--- Logger.h
+++ Logger.h
@@ -16,18 +16,24 @@
 #define LOG_LEVEL_ERROR
 #endif
 
-#ifdef UNICODE
+#if defined(UNICODE) && !defined(FORCE_MULTIBYTE)
 #define tostream std::wostream
 #define tstring std::wstring
 #define tcout std::wcout
 #define vsntprintf _vsnwprintf_s
 #define tcslen wcslen
+#define TXT(str) L ## str
+typedef LPCWCH __PCTCH;
+typedef WCHAR TXTCHAR;
 #else
 #define tostream std::ostream
 #define tstring std::string
 #define tcout std::cout
 #define vsntprintf vsnprintf
 #define tcslen strlen
+#define TXT(str) str
+typedef LPCCH __PCTCH;
+typedef CHAR TXTCHAR;
 #endif
 
 static HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -94,30 +100,30 @@ static const std::map<TCHAR, Color> charToColor{
 	{'6', Color::lightCyan},
 };
 
-const inline Level Level::LEVEL_DEBUG(_T("DEBUG"), Color::lightCyan);
-const inline Level Level::LEVEL_INFO(_T("INFO"), Color::lightGreen);
-const inline Level Level::LEVEL_WARN(_T("WARN"), Color::yellow);
-const inline Level Level::LEVEL_ERROR(_T("ERROR"), Color::lightRed);
+const inline Level Level::LEVEL_DEBUG(TXT("DEBUG"), Color::lightCyan);
+const inline Level Level::LEVEL_INFO(TXT("INFO"), Color::lightGreen);
+const inline Level Level::LEVEL_WARN(TXT("WARN"), Color::yellow);
+const inline Level Level::LEVEL_ERROR(TXT("ERROR"), Color::lightRed);
 
-inline void Log(CONST Level level, PCTCH szSource, PCTCH szFormat, ...) {
+inline void Log(CONST Level level, __PCTCH szSource, __PCTCH szFormat, ...) {
 	va_list args;
 	va_start(args, szFormat);
-	TCHAR msg[MAX_LOG_LEN];
+	TXTCHAR msg[MAX_LOG_LEN];
 	vsntprintf(msg, MAX_LOG_LEN, szFormat, args);
 	va_end(args);
 	tstring szMsg(msg);
 	time_t currentTime = time(nullptr);
 	tm localTime;
 	localtime_s(&localTime, &currentTime);
-	tcout << Color::gray << _T("[")
-		<< Color::white << std::setw(2) << std::setfill(_T('0')) << localTime.tm_hour << std::setfill(_T(' '))
-		<< Color::gray << _T(":")
-		<< Color::white << std::setw(2) << std::setfill(_T('0')) << localTime.tm_min << std::setfill(_T(' '))
-		<< Color::gray << _T(":")
-		<< Color::white << std::setw(2) << std::setfill(_T('0')) << localTime.tm_sec << std::setfill(_T(' '))
-		<< Color::gray << _T("] ")
+	tcout << Color::gray << TXT("[")
+		<< Color::white << std::setw(2) << std::setfill(TXT('0')) << localTime.tm_hour << std::setfill(TXT(' '))
+		<< Color::gray << TXT(":")
+		<< Color::white << std::setw(2) << std::setfill(TXT('0')) << localTime.tm_min << std::setfill(TXT(' '))
+		<< Color::gray << TXT(":")
+		<< Color::white << std::setw(2) << std::setfill(TXT('0')) << localTime.tm_sec << std::setfill(TXT(' '))
+		<< Color::gray << TXT("] ")
 		<< Color::white << szSource << std::setw(15 - tcslen(szSource))
-		<< Color::gray << _T(" - ") << level << Color::gray << _T(" - ");
+		<< Color::gray << TXT(" - ") << level << Color::gray << TXT(" - ");
 	SIZE_T pos = 0;
 	if (szMsg.size() >= 1 && szMsg[0] != COLOR_FORMAT_CHAR) {
 		szMsg.insert(0, 1, DEFAULT_COLOR);
